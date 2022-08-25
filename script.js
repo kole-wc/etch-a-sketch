36b1e0ea3f1251d7bb81276ed2f55e23fb7df162
Kole Chaicharee <kole.chaicharee@gmail.com>
2022-08-25 10:37:04 +0900

    Add function to create squares

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+const board = document.querySelector('.board');
+board.style.gridTemplateColumns = "repeat(16, 1fr)";
+board.style.gridTemplateRows = "repeat(16, 1fr)";
+
+// Generate 16 x 16 grid of square divs
+function createSquares () {
+    for (let i = 0; i < 256; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        board.appendChild(square);
+    }
+}
+
+
+
+
+
+
+
+createSquares();
